cd4686d0e965fd66e14ace129f7775a423911917
oceanway1 <oceanway1@yahoo.ca>
2019-12-10 21:47:34 +0000

    Completed Part 2, fixed minor errors

--- express_server.js
+++ express_server.js
@@ -2,53 +2,44 @@ function generateRandomString(n) {
   let result = '';
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < n; i++) {
-    const random = Math.floor(Math.random() * 100);
+    const random = Math.floor(Math.random() * 60);
     result += chars[random];
   }
   return result;
 };
-// Driver Code  
-// console.log(generateRandomString(6));
-
-
-
-
-
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL /* What goes here? */ };
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString(6);
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`urls/${shortURL}`)
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
