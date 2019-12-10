440fab1463b244a7a225a507f4036c63f5fe4713
oceanway1 <oceanway1@yahoo.ca>
2019-12-10 05:58:58 +0000

    finish URL Shortering Part 1

--- express_server.js
+++ express_server.js
@@ -1,3 +1,19 @@
+function generateRandomString(n) {
+  let result = '';
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < n; i++) {
+    const random = Math.floor(Math.random() * 62);
+    result += chars[random];
+  }
+  return result;
+};
+// Driver Code  
+// console.log(generateRandomString(6));
+
+
+
+
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -8,7 +24,11 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -25,6 +45,10 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
